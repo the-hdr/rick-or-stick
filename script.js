02b2eb49d67c6d845ffa8508a1631569c201cbd5
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-08-27 19:53:58 +0530

    update files

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function generateRandomPicture()
+function getRandomPictureAddress()
 {
-    const randomBit = Math.round(Math.random());
+    // generates a random integer between [0, 1]:
+    const randomBit = Math.floor(2*Math.random());
 
-    const directoryName = 
+    const directoryName = 0 == randomBit ? "rick" : "stick";
+    const filePrefix = directoryName;
+
+    // generates a random integer between [1, 10]:
+    const fileIndex = Math.floor(10*Math.random());
+
+    const fileName = filePrefix + fileIndex + ".jpg";
+    const filePath = "images" + directoryName + "/" + fileName;
+
+    return filePath;
+}
+
+function setRandomPicture()
+{
+    const randomPictureAddress = getRandomPictureAddress();
+    
 }
